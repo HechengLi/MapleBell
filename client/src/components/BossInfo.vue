361ddee15ee17b15fdb6161fb9ef9fac6d716036
Hecheng(Chris) Li <lihecheng93@gmail.com>
2018-11-19 21:07:02 -0500

    separate active and inactive bosses

--- client/src/components/BossInfo.vue
+++ client/src/components/BossInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="list-item__container">
-    <div class="list-item">
+    <div class="list-item" v-bind:class="{ active: active }">
       <h6 class="list-item__title">
         {{bossInfo.boss_name}}
         <span class="list-item__level">Lv{{bossInfo.boss_level}}</span>
@@ -8,7 +8,7 @@
       <div class="list-item__body">
         <p>Location: {{bossInfo.spawn_location}}</p>
         <p>Spawn Time: XX:{{fixed_spawn_time}}</p>
-        <p>Next Spawn: {{timer}}</p>
+        <p>Spawn In: {{timer}}</p>
         <img class="list-item__image" v-bind:src="require(`@/assets/${this.bossInfo.boss_name}.png`)" width="45" height="45" />
       </div>
     </div>
@@ -20,6 +20,9 @@ export default {
   props: {
     bossInfo: {
       type: Object
+    },
+    active: {
+      type: Boolean
     }
   },
   data() {
@@ -35,10 +38,11 @@ export default {
   mounted() {
     const spawnTime = this.bossInfo.spawn_time
     setInterval(() => {
+      console.log(this.active)
       const date = new Date()
       let minsLeft = spawnTime - date.getMinutes() - 1
       minsLeft = minsLeft > 0 ? minsLeft : minsLeft + 60
-      let secsLeft = 60 - date.getSeconds()
+      let secsLeft = 60 - date.getSeconds() - 1
       this.timer = `${minsLeft}m ${secsLeft}s`
     }, 1000)
   }
--- client/src/views/BossSchedule.vue
+++ client/src/views/BossSchedule.vue
@@ -1,6 +1,13 @@
 <template>
   <article class="list-container">
-    <BossInfo v-for="item in data" v-bind:bossInfo="item" v-bind:key="item.boss_name" />
+    <section>
+      <h3 class="list-title list-active">Active</h3>
+      <BossInfo v-for="item in activeBosses" v-bind:bossInfo="item" v-bind:active="true" v-bind:key="item.boss_name" />
+    </section>
+    <section>
+      <h3 class="list-title">Waiting</h3>
+      <BossInfo v-for="item in inactiveBosses" v-bind:bossInfo="item" v-bind:active="false" v-bind:key="item.boss_name" />
+    </section>
   </article>
 </template>
 
@@ -40,6 +47,26 @@ export default {
       .catch(err => {
         console.log('Fetch Error :-S', err)
       })
+  },
+  computed: {
+    activeBosses() {
+      return this.data.filter(n => {
+        const spawnTime = n.spawn_time
+        const date = new Date()
+        let minsLeft = spawnTime - date.getMinutes() - 1
+        minsLeft = minsLeft > 0 ? minsLeft : minsLeft + 60
+        return minsLeft >= 50
+      })
+    },
+    inactiveBosses() {
+      return this.data.filter(n => {
+        const spawnTime = n.spawn_time
+        const date = new Date()
+        let minsLeft = spawnTime - date.getMinutes() - 1
+        minsLeft = minsLeft > 0 ? minsLeft : minsLeft + 60
+        return minsLeft < 50
+      })
+    }
   }
 }
 </script>
@@ -48,5 +75,16 @@ export default {
 .list-container {
   height: 100%;
   overflow: auto;
+
+  .list-title {
+    padding: 5px;
+    padding-bottom: 0;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  .list-active {
+    color: rgb(0, 255, 0);
+  }
 }
 </style>
